64f2636a61b7002cdc0237151924011563987920
lrannabauer <lrannabauer@me.com>
2013-01-06 15:31:33 +0100

    dimensions now working properly

--- src/utils/ParticleGenerator.cpp
+++ src/utils/ParticleGenerator.cpp
@@ -49,17 +49,17 @@ void ParticleGenerator::regularCuboid(ParticleContainer& container, utils::Vecto
 				x[1] = bottomLeft[1] + x2 * h;
 				x[2] = bottomLeft[2] + x3 * h;
 
-				Particle p(x, initialVelocity, m, type,1 ,1);
+				Particle p(x, initialVelocity, m, type,sigma ,epsilon);
 
 				if(brownianMean != 0)
-				MaxwellBoltzmannDistribution(p, brownianMean, Settings::dimensions);
+					MaxwellBoltzmannDistribution(p, brownianMean, Settings::dimensions);
 
 				container.add(p);
 			}
 }
 
 void ParticleGenerator::generateSphere(ParticleContainer& container, utils::Vector<double, 3> center, int radiusSphere,
-										double h, double m , int type, utils::Vector<double, 3> initialVelocity, double brownianMean){
+		double h, double m , int type, utils::Vector<double, 3> initialVelocity, double brownianMean){
 
 	double sigma = Settings::sigma;
 	double epsilon = Settings::epsilon;
@@ -75,42 +75,66 @@ void ParticleGenerator::generateSphere(ParticleContainer& container, utils::Vect
 	std::vector<int> diffs;
 	diffs.push_back(-1);
 	diffs.push_back(1);
+	if(Settings::dimensions == 3){
+		for(int height=0 ; height < radiusSphere; height++){
+			LOG4CXX_TRACE(ParticleGenerator::logger, "Height: \t " << height);
 
-	for(int height=0 ; height < radiusSphere; height++){
-		LOG4CXX_TRACE(ParticleGenerator::logger, "Height: \t " << height);
+			int radiusCircle = floor(sqrt(radiusSphere * radiusSphere - height*height)+0.5) ;
+			LOG4CXX_TRACE(ParticleGenerator::logger, "New sphere radius \t" <<radiusCircle );
 
-		int radiusCircle = floor(sqrt(radiusSphere * radiusSphere - height*height)+0.5) ;
-		LOG4CXX_TRACE(ParticleGenerator::logger, "New sphere radius \t" <<radiusCircle );
+			for(int x1 = 0 ; x1 < radiusCircle; x1++  ){
 
-		for(int x1 = 0 ; x1 < radiusCircle; x1++  ){
+				int boundaryCircle = floor(sqrt(radiusCircle * radiusCircle - x1 * x1) + 0.5);
+				LOG4CXX_TRACE(ParticleGenerator::logger, "New boundary circle \t" <<boundaryCircle );
 
-			int boundaryCircle = floor(sqrt(radiusCircle * radiusCircle - x1 * x1) + 0.5);
-			LOG4CXX_TRACE(ParticleGenerator::logger, "New boundary circle \t" <<boundaryCircle );
+				for(int x2 = 0 ; x2 < boundaryCircle; x2++){
 
-			for(int x2 = 0 ; x2 < boundaryCircle; x2++){
+					for(std::vector<int>::iterator diffX = diffs.begin() ; diffX != diffs.end(); diffX++){
+						for(std::vector<int>::iterator diffY = diffs.begin() ; diffY != diffs.end(); diffY++){
+							for(std::vector<int>::iterator diffZ = diffs.begin() ; diffZ != diffs.end(); diffZ++){
 
+								utils::Vector<double,3> xParticle;
+
+
+								xParticle[0] = center[0] + (h * height) * *diffX ;
+								xParticle[1] = center[1] + (h * x1) * *diffY ;
+								xParticle[2] = center[2] + (h * x2) * *diffZ ;
+
+								Particle p(xParticle , initialVelocity, m ,type ,sigma ,epsilon);
+
+								if(brownianMean != 0)
+									MaxwellBoltzmannDistribution(p, brownianMean, Settings::dimensions);
+								container.add(p);
+								if(x2 == 0)
+									break;
+							}if(x1 == 0)
+								break;
+						}if(height == 0)
+							break;
+					}
+				}
+			}
+		}
+	}else{
+		for(int height=0 ; height < radiusSphere; height++){
+			LOG4CXX_TRACE(ParticleGenerator::logger, "Height: \t " << height);
+			int radiusCircle = floor(sqrt(radiusSphere * radiusSphere - height*height)+0.5) ;
+			LOG4CXX_TRACE(ParticleGenerator::logger, "New sphere radius \t" <<radiusCircle );
+			for(int x1 = 0 ; x1 < radiusCircle; x1++  ){
+				int boundaryCircle = floor(sqrt(radiusCircle * radiusCircle - x1 * x1) + 0.5);
+				LOG4CXX_TRACE(ParticleGenerator::logger, "New boundary circle \t" <<boundaryCircle );
 				for(std::vector<int>::iterator diffX = diffs.begin() ; diffX != diffs.end(); diffX++){
 					for(std::vector<int>::iterator diffY = diffs.begin() ; diffY != diffs.end(); diffY++){
-						for(std::vector<int>::iterator diffZ = diffs.begin() ; diffZ != diffs.end(); diffZ++){
+						utils::Vector<double,3> xParticle;
+						xParticle[0] = center[0] + (h * height) * *diffX ;
+						xParticle[1] = center[1] + (h * x1) * *diffY ;
+						xParticle[2] = center[2] ;
 
-							utils::Vector<double,3> xParticle;
+						Particle p(xParticle , initialVelocity, m ,type ,sigma ,epsilon);
 
-
-							xParticle[0] = center[0] + (h * height) * *diffX ;
-							xParticle[1] = center[1] + (h * x1) * *diffY ;
-							xParticle[2] = center[2];
-							if(Settings::dimensions == 3)
-								xParticle[2] += (h * x2) * *diffZ ;
-
-							Particle p(xParticle , initialVelocity, m ,type ,1 ,1);
-
-							if(brownianMean != 0)
+						if(brownianMean != 0)
 							MaxwellBoltzmannDistribution(p, brownianMean, Settings::dimensions);
-
-							container.add(p);
-							if((Settings::dimensions == 2)||(x2 == 0))
-								break;
-						}
+						container.add(p);
 						if(x1 == 0)
 							break;
 					}
@@ -119,5 +143,7 @@ void ParticleGenerator::generateSphere(ParticleContainer& container, utils::Vect
 				}
 			}
 		}
+
 	}
+
 }
