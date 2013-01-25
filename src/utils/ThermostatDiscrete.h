57e50fa1f031fed2031918d4a140f0c99c6b774c
lrannabauer <lrannabauer@me.com>
2013-01-26 00:00:36 +0100

    implemented thermostat descrete

---
+++ src/utils/ThermostatDiscrete.h
@@ -0,0 +1,35 @@
+/*
+ * ThermostatDiscrete.h
+ *
+ *  Created on: 24.01.2013
+ *      Author: Leo
+ */
+
+#ifndef THERMOSTATDISCRETE_H_
+#define THERMOSTATDISCRETE_H_
+#include "ParticleContainer.h"
+#include <utils/Settings.h>
+
+#define BOLTZMANN 0.000000000000000000000013806503
+
+class ThermostatDiscrete {
+public:
+	ThermostatDiscrete();
+	static void updateThermostate(ParticleContainer *particles);
+
+	static double beta;
+	static double currentEnergy;
+	static double energyPerStep;
+	static double numberOfParticles;
+	static double currentTemperature;
+	static double targetEnergy;
+
+private:
+	static log4cxx::LoggerPtr logger;
+	static void getStepEnergy();
+	static void calculateCurrentEnergy(ParticleContainer* particles);
+	static void setCurrentTemperature();
+
+};
+
+#endif /* THERMOSTATDISCRETE_H_ */
