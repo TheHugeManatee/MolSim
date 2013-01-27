653ec1edfc044869b949de7dcf6719f2edadc3fe
Jakob Weiss <jakob.jw.weiss@googlemail.com>
2013-01-27 20:57:51 +0100

    * first step

--- src/JobQueue.h
+++ src/JobQueue.h
@@ -66,8 +66,14 @@ class Job {
 private:
 	omp_lock_t lock_;
 
+protected:
+	int requiredDependencies;
+	int dependenciesFinished;
+
 public:
-	Job() 			{	omp_init_lock(&lock_);	};
+	Job(int requiredDependencies_ = 0) : requiredDependencies(requiredDependencies_), dependenciesFinished(0)		{
+		omp_init_lock(&lock_);
+	};
 	virtual ~Job() 	{	omp_destroy_lock(&lock_);};
 
 	void lock() 	{	omp_set_lock(&lock_);	};
@@ -76,57 +82,21 @@ public:
 
 	virtual void exec(CellListContainer *container, SimulationScenario *scenario) = 0;
 	virtual void enqueueDependentJobs(JobQueue &queue) {};
-	virtual bool reset() { return true; };
-};
-
-
-class BlockJob : public Job {
-protected:
-	const int start, end;
-	SliceJob *firstSlice;
-	SliceJob *succeedingSlice;
-
-public:
-	BlockJob(int start_, int end_, SliceJob *firstSlice_, SliceJob *succeedingSlice_) :
-		start(start_), end(end_), firstSlice(firstSlice_), succeedingSlice(succeedingSlice_) {};
-
-	void enqueueDependentJobs(JobQueue &queue);
-};
-
-class SliceJob : public Job {
-protected:
-	const int sliceIdx;
-
-public:
-	bool containerDone;
-	bool predecessorDone;
-	SliceJob(int sliceIdx_) : sliceIdx(sliceIdx_), containerDone(false), predecessorDone(false) {};
-
-	void enqueueDepenedentJobs(JobQueue &queue) {	/*no follow-ups*/};
-	bool reset() {
-		containerDone = false;
-		predecessorDone = false;
-		return false;
+	virtual bool reset() {
+		dependenciesFinished = 0;
+		return !requiredDependencies;
+	};
+	virtual void dependencyFinished(JobQueue &queue) {
+		lock();
+		if(++dependenciesFinished >= requiredDependencies) {
+			queue.enqueue(this);
+		}
+		unlock();
 	};
 };
 
 
-class BlockJobX0 : public BlockJob {
-public:
-	BlockJobX0(int start_, int end_, SliceJob *firstSlice_, SliceJob *succeedingSlice_) :
-		BlockJob(start_, end_, firstSlice_, succeedingSlice_) {	};
 
-	void exec(CellListContainer *container, SimulationScenario *scenario);
-};
-
-class SliceJobX0 : public SliceJob {
-public:
-	SliceJobX0(int sliceIdx_) : SliceJob(sliceIdx_) {};
-
-	void exec(CellListContainer *container, SimulationScenario *scenario);
-	void enqueueDependentJobs(JobQueue &queue) {};
-
-};
 
 #endif /* _OPENMP */
 
--- src/SimulationJobQueue.h
+++ src/SimulationJobQueue.h
@@ -16,4 +16,46 @@ public:
 	~SimulationJobQueue();
 };
 
+class BlockJob : public Job {
+protected:
+	const int start, end;
+	SliceJob *firstSlice;
+	SliceJob *succeedingSlice;
+
+public:
+	BlockJob(int start_, int end_, SliceJob *firstSlice_, SliceJob *succeedingSlice_) :
+		start(start_), end(end_), firstSlice(firstSlice_), succeedingSlice(succeedingSlice_) {};
+
+	void enqueueDependentJobs(JobQueue &queue);
+};
+
+class SliceJob : public Job {
+protected:
+	const int sliceIdx;
+
+public:
+
+	SliceJob(int sliceIdx_) : Job(2), sliceIdx(sliceIdx_) {};
+
+	void enqueueDepenedentJobs(JobQueue &queue) {	/*no follow-ups*/};
+};
+
+
+class BlockJobX0 : public BlockJob {
+public:
+	BlockJobX0(int start_, int end_, SliceJob *firstSlice_, SliceJob *succeedingSlice_) :
+		BlockJob(start_, end_, firstSlice_, succeedingSlice_) {	};
+
+	void exec(CellListContainer *container, SimulationScenario *scenario);
+};
+
+class SliceJobX0 : public SliceJob {
+public:
+	SliceJobX0(int sliceIdx_) : SliceJob(sliceIdx_) {};
+
+	void exec(CellListContainer *container, SimulationScenario *scenario);
+	void enqueueDependentJobs(JobQueue &queue) {};
+
+};
+
 #endif /* SIMULATIONJOBQUEUE_H_ */
